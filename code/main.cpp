f5bd3bd1491b067988361507f58296924de08904
DontStopThinking <infoshantanu02@gmail.com>
2024-10-24 14:41:19 +0100

    Process Window messages
    As an example, implement 'C' to change window title

--- code/main.cpp
+++ code/main.cpp
@@ -1,5 +1,26 @@
 #include <Windows.h>
 
+static LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_CLOSE:
+    {
+        PostQuitMessage(0);
+    } break;
+
+    case WM_KEYDOWN:
+    {
+        if (wParam == 'C')
+        {
+            SetWindowText(hWnd, L"New Window Title");
+        }
+    } break;
+    }
+
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, PSTR /*lpCmdLine*/, int /*nCmdShow*/)
 {
     const LPCWSTR className = L"hw3d";
@@ -9,7 +30,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, PSTR /*lp
     {
         .cbSize = sizeof(wc),
         .style = CS_OWNDC,
-        .lpfnWndProc = DefWindowProc,
+        .lpfnWndProc = WndProc,
         .hInstance = hInstance,
         .lpszClassName = className
     };
@@ -19,7 +40,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, PSTR /*lp
     const LPCWSTR windowTitle = L"HW3D Window";
 
     // Create window instance
-    HWND hwnd = CreateWindowEx(
+    HWND hWnd = CreateWindowEx(
         0,
         className,
         windowTitle, // window title
@@ -34,14 +55,30 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, PSTR /*lp
         nullptr // lpParam
     );
 
-    if (!hwnd)
+    if (!hWnd)
     {
         return -1;
     }
 
-    ShowWindow(hwnd, SW_SHOW);
+    ShowWindow(hWnd, SW_SHOW);
 
-    while (true);
+    // Message pump
+    MSG msg;
+    BOOL gResult;
 
-    return 0;
+    // If GetMessage returns negative value, then that means there was an error
+    while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    if (gResult == -1)
+    {
+        return -1;
+    }
+    else
+    {
+        return (int)msg.wParam;  // This is the value returned from WndProc
+    }
 }
