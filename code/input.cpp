77a2456a532d92f1d01c0681bbdccb17e06c01fc
Shantanu Balse <infoshantanu02@gmail.com>
2024-11-09 17:45:17 +0000

    Minor: Move some code around

--- code/input.cpp
+++ code/input.cpp
@@ -11,19 +11,6 @@ constinit std::bitset<NUMBUTTONS> s_ButtonState; //! NOTE(sbalse): 1 = button he
 constinit std::bitset<NUMBUTTONS> s_ButtonDowns; //! NOTE(sbalse): 1 = button pressed this frame.
 constinit std::bitset<NUMBUTTONS> s_ButtonUps; //! NOTE(sbalse): 1 = button released this frame.
 
-void InputUpdate(uint8 button, bool pressed)
-{
-    s_ButtonState[button] = pressed;
-    if (pressed)
-    {
-        s_ButtonDowns[button] = true;
-    }
-    else
-    {
-        s_ButtonUps[button] = true;
-    }
-}
-
 bool KeyboardButtonCheck(uint8 button)
 {
     return s_ButtonState[button];
@@ -52,4 +39,17 @@ void InputEndFrame()
     s_PrevButtonState = s_ButtonState;
     s_ButtonDowns.reset();
     s_ButtonUps.reset();
+}
+
+void InputUpdate(uint8 button, bool pressed)
+{
+    s_ButtonState[button] = pressed;
+    if (pressed)
+    {
+        s_ButtonDowns[button] = true;
+    }
+    else
+    {
+        s_ButtonUps[button] = true;
+    }
 }
--- code/input.h
+++ code/input.h
@@ -1,9 +1,10 @@
 #pragma once
 #include "types.h"
 
-void InputUpdate(uint8 button, bool pressed);
 bool KeyboardButtonCheck(uint8 button);
 bool KeyboardButtonPressed(uint8 button);
 bool KeyboardButtonReleased(uint8 button);
+
 void InputClear();
 void InputEndFrame();
+void InputUpdate(uint8 button, bool pressed);
