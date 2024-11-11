e23aba6f98163008d6291f84c4758eb1aabda0e4
Shantanu Balse <infoshantanu02@gmail.com>
2024-11-11 14:58:07 +0000

    Update todo.h

--- code/todo.h
+++ code/todo.h
@@ -1,8 +1,9 @@
 /*
-[o] Mouse input.
-    [] Mouse check for checking if a mouse button is held down. Might need to create a bitset for the three
-       mouse buttons to make the code less cumbersome.
 [] Timing.
 [] Multi-window.
 [] DirectX.
+
+[] Input improvement
+    [] create concept of an "action" so we can bind multiple input buttons to the same
+       action.
 */
